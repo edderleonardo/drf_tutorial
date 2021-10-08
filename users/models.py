b3d136d56009d4c276add3a0be6af1845817c190
edderleonardo <edder@alluxi.com>
2021-10-07 23:11:33 -0500

    add profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    total_fortune = models.DecimalField(max_digits=19, decimal_places=10)
+
+    def __str__(self):
+        return self.user
